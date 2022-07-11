418f69f56afc99ce6ef289683ea2995c067e17b6
n4inz <nainmo@ymail.com>
2022-07-11 15:59:01 +0800

    overview task 80%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" /> --}}
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" />
     {{-- <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script> --}}
    
     <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
@@ -18,27 +18,21 @@
 </head>
 <body>
 
-    <label class="inline-flex items-center">
-        <input type="checkbox" class="form-checkbox text-green-500" checked>
-        <span class="ml-2">ssd</span>
-      </label>
-
-<input style="color: #3BD7CF" type="checkbox" value="" class="w-5 h-5 rounded bg-gray-100 text-[#3BD7CF] border-none outline-none focus:outline:none focus:ring-transparent focus:border-current focus:ring-0" >
-
-  <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+    
+<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
         <li class="mr-2" role="presentation">
-            <button class="inline-block p-4 rounded-t-lg border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500" id="profile" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
+            <button style="color: '#3BD7CF'" class="inline-block p-4 rounded-t-lg border-b-2 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500" id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
         </li>
         <li class="mr-2" role="presentation">
             <button class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700" id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Dashboard</button>
         </li>
-        {{-- <li class="mr-2" role="presentation">
+        <li class="mr-2" role="presentation">
             <button class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
         </li>
         <li role="presentation">
             <button class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700" id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
-        </li> --}}
+        </li>
     </ul>
 </div>
 <div id="myTabContent">
@@ -48,71 +42,55 @@
     <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
         <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
     </div>
-    {{-- <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
+    <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings" role="tabpanel" aria-labelledby="settings-tab">
         <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
     </div>
     <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
         <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
-    </div> --}}
+    </div>
 </div>
 
-<div class="flex items-center mr-4">
-    <input style="color: #3BD7CF" id="sss-checkbox" type="checkbox" value="" class="w-5 h-5 rounded bg-gray-200 border-none outline-none focus:outline:none focus:ring-transparent focus:border-current focus:ring-0" >
-</div>
 
-<label class="checkbox">Satu
-    <input type="checkbox" checked="checked">
-    <span class="check"></span>
-</label>
-<label class="checkbox">Dua
-    <input type="checkbox">
-    <span class="check"></span>
-</label>
-<label class="checkbox">Tiga
-    <input type="checkbox">
-    <span class="check"></span>
-</label>
-<label class="checkbox">Empat
-    <input type="checkbox">
-    <span class="check"></span>
-</label>
+
 
 
 <script>
   const tabElements = [
     {
         id: 'profile',
-        triggerEl: document.querySelector('#profile'),
+        triggerEl: document.querySelector('#profile-tab'),
         targetEl: document.querySelector('#profile')
     },
-    // {
-    //     id: 'dashboard',
-    //     triggerEl: document.querySelector('#dashboard-tab-example'),
-    //     targetEl: document.querySelector('#dashboard-example')
-    // },
-    // {
-    //     id: 'settings',
-    //     triggerEl: document.querySelector('#settings-tab-example'),
-    //     targetEl: document.querySelector('#settings-example')
-    // },
-    // {
-    //     id: 'contacts',
-    //     triggerEl: document.querySelector('#contacts-tab-example'),
-    //     targetEl: document.querySelector('#contacts-example')
-    // }
+    {
+        id: 'dashboard',
+        triggerEl: document.querySelector('#dashboard-tab'),
+        targetEl: document.querySelector('#dashboard')
+    },
+    {
+        id: 'settings',
+        triggerEl: document.querySelector('#settings-tab'),
+        targetEl: document.querySelector('#settings')
+    },
+    {
+        id: 'contacts',
+        triggerEl: document.querySelector('#contacts-tab'),
+        targetEl: document.querySelector('#contacts')
+    }
 ];
 
 // options with default values
 const options = {
-    defaultTabId: 'settings',
-    activeClasses: 'text-red-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-red-600 dark:border-blue-500',
-    inactiveClasses: 'text-red-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+    defaultTabId: 'profile-tab',
+    activeClasses: 'text-green-500 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-red-600 dark:border-blue-500',
+    inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: () => {
-        console.log('tab is shown');
+       
     }
-};
+    
+}
 
 const tabs = new Tabs(tabElements, options);
+    console.log(tabs.getActiveTab())
 </script>
   
 
