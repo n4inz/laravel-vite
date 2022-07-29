1bfe8016259273d50b1a2455dd8f8ef734b64ad8
n4inz <nainmo@ymail.com>
2022-07-29 11:17:46 +0800

    install spatie permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Multitenancy\Models\Tenant;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    use HasRoles
 
     /**
      * The attributes that are mass assignable.
