e4485a33dd9c0f9297dd62c87f767492e921569d
n4inz <nainmo@ymail.com>
2022-08-02 20:40:48 +0800

    fix bug setting avatar

--- app/Repositories/SettingRepository.php
+++ app/Repositories/SettingRepository.php
@@ -9,22 +9,27 @@ use App\Models\SettingGeneral;
 use App\Models\SettingServiceCategory;
 use App\Models\SettingServiceLocationFee;
 use App\Models\SettingUsers;
+use App\Models\User;
 
 class SettingRepository {
 
     public function created($request)
     {
+        User::where('id', auth()->user()->id)->update([
+            'full_name' => $request->agency_name,
+        ]);
+        
         SettingGeneral::create([
             'agency_name' => $request->agency_name,
             'url_ending_legal' => $request->url_ending_legal, 
             'company_description' => $request->company_description,
             'avatar' => null,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
     
         $setting_detail =  SettingDetail::create([
             'service_type' => $request->service_type,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
     
         $setting_detail->service_location_fee()->create([
@@ -33,7 +38,7 @@ class SettingRepository {
             'aplication_fee' => $request->aplication_fee,
             'placement_fee' => $request->placement_fee,
             'hourly_rate' => $request->hourly_rate,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
     
         $setting_detail->service_category()->create([
@@ -50,27 +55,27 @@ class SettingRepository {
             'house_cleaning' => $request->house_cleaning,
             'private_home_kitchen' => $request->private_home_kitchen,
             'other' => $request->other,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
         
         SettingUsers::create([
             'client' => $request->client,
             'caregives' => $request->caregives,
             'interested_public_pool' => $request->interested_public_pool,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
     
         $setting_additionals = SettingAdditionals::create([
             'notification_email' => $request->notification_email,
             'dashboard_color_theme' => $request->dashboard_color_theme,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
 
         foreach($request->body as $key => $value){
             if($request->body[$key] != null){
                 $setting_additionals->defined_check_list()->create([
                     'body' => $request->body[$key],
-                    'users_id' => auth()->guard('web')->user()->id
+                    'users_id' => auth()->user()->id
                 ]);
             }
         }
@@ -79,20 +84,23 @@ class SettingRepository {
 
     public function updated($request)
     {
-        SettingGeneral::where('users_id' , auth()->guard('web')->user()->id)->update([
+        User::where('id', auth()->user()->id)->update([
+            'full_name' => $request->agency_name,
+        ]);
+        SettingGeneral::where('users_id' , auth()->user()->id)->update([
             'agency_name' => $request->agency_name,
             'url_ending_legal' => $request->url_ending_legal, 
             'company_description' => $request->company_description,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
 
-        SettingDetail::where('users_id' , auth()->guard('web')->user()->id)->update([
+        SettingDetail::where('users_id' , auth()->user()->id)->update([
             'service_type' => $request->service_type,
-            'users_id' => auth()->guard('web')->user()->id
+            'users_id' => auth()->user()->id
         ]);
 
         
-        SettingServiceLocationFee::where('users_id' , auth()->guard('web')->user()->id)->update([
+        SettingServiceLocationFee::where('users_id' , auth()->user()->id)->update([
             'location' => $request->location,
             'send_notifcation' => $request->send_notifcation,
             'aplication_fee' => $request->aplication_fee,
@@ -100,7 +108,7 @@ class SettingRepository {
             'hourly_rate' => $request->hourly_rate
         ]);
 
-        SettingServiceCategory::where('users_id' , auth()->guard('web')->user()->id)->update([
+        SettingServiceCategory::where('users_id' , auth()->user()->id)->update([
             'chile_care' => $request->chile_care,
             'nanny_sister' => $request->nanny_sister,
             'au_pair' => $request->au_pair,
@@ -116,13 +124,13 @@ class SettingRepository {
             'other' => $request->other
         ]);
 
-        SettingUsers::where('users_id' , auth()->guard('web')->user()->id)->update([
+        SettingUsers::where('users_id' , auth()->user()->id)->update([
             'client' => $request->client,
             'caregives' => $request->caregives,
             'interested_public_pool' => $request->interested_public_pool,
         ]);
 
-        $sett_id = SettingAdditionals::updateOrCreate(['users_id' => auth()->guard('web')->user()->id], [
+        $sett_id = SettingAdditionals::updateOrCreate(['users_id' => auth()->user()->id], [
             'notification_email' => $request->notification_email,
             'dashboard_color_theme' => $request->dashboard_color_theme,
         ]);
@@ -132,7 +140,7 @@ class SettingRepository {
                 SettingDefinedCheckList::create([
                     'body' => $request->body[$keys],
                     'setting_additionals_id' => $sett_id->id,
-                    'users_id' => auth()->guard('web')->user()->id
+                    'users_id' => auth()->user()->id
                 ]);
             }
         }
